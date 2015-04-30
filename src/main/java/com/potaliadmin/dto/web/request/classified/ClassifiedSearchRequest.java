491f3abd5753772f0e858372ee05ac9f26aeb46f
= <=>
2015-04-30 22:45:22 +0530

    done

---
+++ src/main/java/com/potaliadmin/dto/web/request/classified/ClassifiedSearchRequest.java
@@ -0,0 +1,93 @@
+package com.potaliadmin.dto.web.request.classified;
+
+import com.potaliadmin.constants.query.EnumSearchOperation;
+import com.potaliadmin.dto.web.request.framework.GenericRequest;
+
+/**
+ * Created by shaktsin on 4/30/15.
+ */
+public class ClassifiedSearchRequest extends GenericRequest {
+
+  String locationFilter;
+  String primaryCatFilter;
+  String secondaryCatFilter;
+  String circleFilter;
+  Long postId;
+  int operation;
+  int perPage;
+  int pageNo;
+
+
+  @Override
+  public boolean validate() {
+    boolean isValid = super.validate();
+    if (isValid && EnumSearchOperation.contains(operation) && postId == null) {
+      isValid = false;
+    }
+    return isValid;
+  }
+
+  public String getLocationFilter() {
+    return locationFilter;
+  }
+
+  public void setLocationFilter(String locationFilter) {
+    this.locationFilter = locationFilter;
+  }
+
+  public String getPrimaryCatFilter() {
+    return primaryCatFilter;
+  }
+
+  public void setPrimaryCatFilter(String primaryCatFilter) {
+    this.primaryCatFilter = primaryCatFilter;
+  }
+
+  public String getSecondaryCatFilter() {
+    return secondaryCatFilter;
+  }
+
+  public void setSecondaryCatFilter(String secondaryCatFilter) {
+    this.secondaryCatFilter = secondaryCatFilter;
+  }
+
+  public String getCircleFilter() {
+    return circleFilter;
+  }
+
+  public void setCircleFilter(String circleFilter) {
+    this.circleFilter = circleFilter;
+  }
+
+  public Long getPostId() {
+    return postId;
+  }
+
+  public void setPostId(Long postId) {
+    this.postId = postId;
+  }
+
+  public int getOperation() {
+    return operation;
+  }
+
+  public void setOperation(int operation) {
+    this.operation = operation;
+  }
+
+  public int getPerPage() {
+    return perPage;
+  }
+
+  public void setPerPage(int perPage) {
+    this.perPage = perPage;
+  }
+
+  public int getPageNo() {
+    return pageNo;
+  }
+
+  public void setPageNo(int pageNo) {
+    this.pageNo = pageNo;
+  }
+}
--- src/main/java/com/potaliadmin/resources/classified/ClassifiedResource.java
+++ src/main/java/com/potaliadmin/resources/classified/ClassifiedResource.java
@@ -1,13 +1,22 @@
 package com.potaliadmin.resources.classified;
 
+import com.potaliadmin.constants.DefaultConstants;
+import com.potaliadmin.constants.query.EnumSearchOperation;
+import com.potaliadmin.constants.request.RequestConstants;
+import com.potaliadmin.dto.web.request.classified.ClassifiedEditRequest;
 import com.potaliadmin.dto.web.request.classified.ClassifiedPostRequest;
+import com.potaliadmin.dto.web.request.classified.ClassifiedSearchRequest;
 import com.potaliadmin.dto.web.request.jobs.JobCreateRequest;
+import com.potaliadmin.dto.web.request.jobs.JobEditRequest;
 import com.potaliadmin.dto.web.response.classified.ClassifiedPostResponse;
+import com.potaliadmin.dto.web.response.classified.ClassifiedSearchResponse;
 import com.potaliadmin.dto.web.response.classified.PrepareClassifiedResponse;
 import com.potaliadmin.dto.web.response.job.JobResponse;
 import com.potaliadmin.dto.web.response.newsfeed.PrepareNewsFeedResponse;
 import com.potaliadmin.pact.service.classified.ClassifiedService;
+import com.potaliadmin.util.BaseUtil;
 import com.potaliadmin.util.rest.InputParserUtil;
+import org.apache.commons.lang.StringUtils;
 import org.apache.shiro.authz.annotation.RequiresAuthentication;
 import org.glassfish.jersey.media.multipart.FormDataBodyPart;
 import org.glassfish.jersey.media.multipart.FormDataParam;
@@ -48,7 +57,7 @@ public class ClassifiedResource {
   @Consumes(MediaType.MULTIPART_FORM_DATA)
   @Produces("application/json")
   @RequiresAuthentication
-  public ClassifiedPostResponse createJob(@FormDataParam("classified") FormDataBodyPart classified,
+  public ClassifiedPostResponse createClassified(@FormDataParam("classified") FormDataBodyPart classified,
                                @FormDataParam("iFile") List<FormDataBodyPart> imgFiles,
                                @FormDataParam("jFile") List<FormDataBodyPart> jFile) {
     try {
@@ -60,6 +69,95 @@ public class ClassifiedResource {
 
       //return getJobService().createJob(createJobRequest, imgFiles, jFile);
       return getClassifiedService().createClassifiedPost(createJobRequest, imgFiles, jFile);
+    } catch (Exception e) {
+      ClassifiedPostResponse classifiedPostResponse = new ClassifiedPostResponse();
+      classifiedPostResponse.setException(Boolean.TRUE);
+      classifiedPostResponse.addMessage(e.getMessage());
+      return classifiedPostResponse;
+    }
+  }
+
+  @GET
+  @Path("/{id}")
+  @Produces("application/json")
+  @RequiresAuthentication
+  public ClassifiedPostResponse getClassified(@PathParam(RequestConstants.ID) Long id) {
+    try {
+      return getClassifiedService().getClassified(id);
+    } catch (Exception e) {
+      ClassifiedPostResponse classifiedPostResponse = new ClassifiedPostResponse();
+      classifiedPostResponse.setException(Boolean.TRUE);
+      classifiedPostResponse.addMessage(e.getMessage());
+      return classifiedPostResponse;
+    }
+  }
+
+
+  @POST
+  @Path("/list")
+  @Produces("application/json")
+  @RequiresAuthentication
+  public ClassifiedSearchResponse getJobs(ClassifiedSearchRequest classifiedSearchRequest){
+
+    String[] locationFilterList=null;
+    String[] primaryCatList = null;
+    String[] secondaryCatList=null;
+    String[] salaryFilterList=null;
+    String[] experienceFilterList=null;
+    String[] circleFilterList = null;
+    try {
+      if (StringUtils.isNotBlank(classifiedSearchRequest.getLocationFilter())) {
+        locationFilterList = classifiedSearchRequest.getLocationFilter().split(DefaultConstants.REQUEST_SEPARATOR);
+      }
+
+      if (StringUtils.isNotBlank(classifiedSearchRequest.getPrimaryCatFilter())) {
+        primaryCatList = classifiedSearchRequest.getPrimaryCatFilter().split(DefaultConstants.REQUEST_SEPARATOR);
+      }
+
+      if (StringUtils.isNotBlank(classifiedSearchRequest.getSecondaryCatFilter())) {
+        secondaryCatList = classifiedSearchRequest.getSecondaryCatFilter().split(DefaultConstants.REQUEST_SEPARATOR);
+      }
+
+      if (StringUtils.isNotBlank(classifiedSearchRequest.getCircleFilter())) {
+        circleFilterList = classifiedSearchRequest.getCircleFilter().split(DefaultConstants.REQUEST_SEPARATOR);
+      }
+
+      EnumSearchOperation enumSearchOperation = EnumSearchOperation.getById(classifiedSearchRequest.getOperation());
+      Long postId = null;
+      if (enumSearchOperation != null) {
+        //date = DateUtils.convertFromString(jobSearchRequest.getPostDate());
+        postId = classifiedSearchRequest.getPostId();
+      }
+
+      return getClassifiedService().searchClassified(BaseUtil.convertToLong(circleFilterList),
+          BaseUtil.convertToLong(locationFilterList), BaseUtil.convertToLong(primaryCatList),
+          BaseUtil.convertToLong(secondaryCatList), enumSearchOperation, postId, classifiedSearchRequest.getPerPage(),
+          classifiedSearchRequest.getPageNo());
+
+
+
+
+    } catch (Exception e) {
+      ClassifiedSearchResponse classifiedSearchResponse = new ClassifiedSearchResponse();
+      classifiedSearchResponse.setException(true);
+      classifiedSearchResponse.addMessage("Some internal exception occurred");
+      return classifiedSearchResponse;
+    }
+  }
+
+  @POST
+  @Path("/edit")
+  @Consumes(MediaType.MULTIPART_FORM_DATA)
+  @Produces("application/json")
+  @RequiresAuthentication
+  public ClassifiedPostResponse editJob(@FormDataParam("classified") FormDataBodyPart classified,
+                             @FormDataParam("iFile") List<FormDataBodyPart> imgFiles,
+                             @FormDataParam("jFile") List<FormDataBodyPart> jFiles
+  ) {
+    try {
+      ClassifiedEditRequest classifiedEditRequest = (ClassifiedEditRequest)
+          InputParserUtil.parseMultiPartObject(classified.getValue(), ClassifiedEditRequest.class);
+      return getClassifiedService().editClassifiedPost(classifiedEditRequest, imgFiles, jFiles);
     } catch (Exception e) {
       ClassifiedPostResponse classifiedPostResponse = new ClassifiedPostResponse();
       classifiedPostResponse.setException(Boolean.TRUE);
